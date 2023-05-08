c9f28dcb97d82f2b8d8fd28fc95f049bd4054f7b
vijay <VijayIyer>
2023-05-07 23:38:25 -0400

    added initial page for adding sections in resume

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,82 +1,60 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Contact from "./components/contact";
-import Education from "./components/education";
-import Experience from "./components/experience";
-import PageNotFound from "./components/pageNotFound";
-import Projects from "./components/projects";
-import Skills from "./components/skills";
-import InfoPage from "./components/infoPage";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
-import ResumeBuilder from "./components/resumeBuilder";
 function App() {
   return (
     <>
-      <div className='App'>
-        <BrowserRouter>
-          <Navbar bg='dark' variant='dark' fixed='top'>
-            <Container>
-              <Navbar.Brand href='/'>
-                <img
-                  src='/images/resume-icon-png-19025.png'
-                  width='30'
-                  height='30'
-                  className='d-inline-block align-top'
-                  alt='Home'
-                />
-              </Navbar.Brand>
-              <Nav className='me-auto'>
-                <NavDropdown title='Build Profile'>
-                  <NavDropdown.Item>
-                    <Link to='/contact' style={{ textDecoration: "none" }}>
-                      Contact
-                    </Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    {" "}
-                    <Link to='/experience' style={{ textDecoration: "none" }}>
-                      Experience
-                    </Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to='/skills' style={{ textDecoration: "none" }}>
-                      Skills
-                    </Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to='/projects' style={{ textDecoration: "none" }}>
-                      Projects
-                    </Link>
-                  </NavDropdown.Item>
-
-                  <NavDropdown.Item>
-                    <Link to='/education' style={{ textDecoration: "none" }}>
-                      Education
-                    </Link>
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href='/builder'>Build Resume</Nav.Link>
-              </Nav>
-            </Container>
-          </Navbar>
-          <Container>
-            <Routes>
-              <Route path='/' element={<InfoPage />} />
-              <Route path='/education' element={<Education />} />
-              <Route path='/skills' element={<Skills />} />
-              <Route path='/experience' element={<Experience />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='/projects' element={<Projects />} />
-              <Route path='/builder' element={<ResumeBuilder />} />
-              <Route path='*' element={<PageNotFound />} />
-            </Routes>
-          </Container>
-        </BrowserRouter>
-      </div>
+      
+        <Container fluid className="vh-100">
+          <Row className="justify-content-md-center">
+            <Col md={8} className="border m-1">
+              <Container fluid>
+                  <Row>
+                    <Col className="p-1">
+                      <div role="button" className="p-3 border text-center">+ Add Experience Section</div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="p-1">
+                      <div role="button" className="p-3 border text-center">+ Add Projects Section</div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="p-1">
+                      <div role="button" className="p-3 border text-center">+ Add Skill Section</div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="p-1">
+                    <div role="button" className="p-3 border text-center">
+                      + Add Education Section
+                    </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="p-1">
+                    <div role="button" className="p-3 border text-center">
+                      + Add Contact Section
+                    </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="p-1">
+                    <div role="button" className="p-3 border text-center">
+                      + Add Hobbies Section
+                    </div>
+                    </Col>
+                  </Row>
+              </Container>
+            </Col>
+            <Col className="border text-center">
+                Preview
+            </Col>
+            </Row>
+        </Container>
+      
     </>
   );
 }
