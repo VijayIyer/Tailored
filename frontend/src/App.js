7223155dcdd095a5aa97e1f05959df920407c2d1
vijay <VijayIyer>
2023-05-29 08:50:53 -0400

    Adds landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,158 +1,59 @@
-import { useState } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import "bootstrap/dist/css/bootstrap.css";
-import Experience from "./components/experience";
-import Project from "./components/projects";
-import Skills from "./components/skills";
-import Education from "./components/education";
-import Contact from "./components/contact";
-import "./styles.css";
-function App() {
-  const [experienceReplaced, setExperienceReplaced] = useState(false);
-  const [projectReplaced, setProjectReplaced] = useState(false);
-  const [skillReplaced, setSkillReplaced] = useState(false);
-  const [educationReplaced, setEducationReplaced] = useState(false);
-  const [contactReplaced, setContactReplaced] = useState(false);
-  const replaceSection = (sectionName) => {
-    switch (sectionName) {
-      case "experience":
-        if (!experienceReplaced) setExperienceReplaced(true);
-        break;
-      case "project":
-        if (!projectReplaced) setProjectReplaced(true);
-        break;
-      case "skill":
-        if (!skillReplaced) setSkillReplaced(true);
-        break;
-      case "education":
-        if (!educationReplaced) setEducationReplaced(true);
-        break;
-      case "contact":
-        if (!contactReplaced) setContactReplaced(true);
-        break;
-      default:
-        return;
-    }
-  };
-  return (
-    <>
-      <Container fluid className='vh-100'>
-        <Row className='text-center'>
-          <h1>Create Profile</h1>
-        </Row>
-        <Row className='justify-content-md-center'>
-          <Col md={8} className='border border-dark m-1'>
-            <Container fluid>
-              <Row
-                className='section'
-                onClick={() => replaceSection("contact")}
-              >
-                <Col className='p-1'>
-                  {contactReplaced ? (
-                    <Contact />
-                  ) : (
-                    <div
-                      role='button'
-                      className='section__placeholder p-3 text-center'
-                    >
-                      + Add Contact Section
-                    </div>
-                  )}
-                </Col>
-              </Row>
-              <Row
-                className='section'
-                onClick={() => replaceSection("education")}
-              >
-                <Col className='p-1 text-center'>
-                  {educationReplaced ? (
-                    <Education />
-                  ) : (
-                    <div
-                      role='button'
-                      className='section__placeholder p-3 text-center'
-                    >
-                      + Add Education Section
-                    </div>
-                  )}
-                </Col>
-              </Row>
-              <Row className='section' onClick={() => replaceSection("skill")}>
-                {skillReplaced ? (
-                  <Col className='p-1 text-center'>
-                    <Skills />
-                  </Col>
-                ) : (
-                  <Col className='p-1'>
-                    <div
-                      role='button'
-                      className='section__placeholder p-3 text-center'
-                    >
-                      + Add Skill Section
-                    </div>
-                  </Col>
-                )}
-              </Row>
-              <Row
-                className='section'
-                onClick={() => replaceSection("experience")}
-              >
-                {experienceReplaced ? (
-                  <Col className='p-1 text-center'>
-                    <Experience />
-                  </Col>
-                ) : (
-                  <Col className='p-1'>
-                    <div
-                      role='button'
-                      className='section__placeholder p-3 text-center'
-                    >
-                      + Add Experience Section
-                    </div>
-                  </Col>
-                )}
-              </Row>
-              <Row
-                className='section'
-                onClick={() => replaceSection("project")}
-              >
-                {projectReplaced ? (
-                  <Col className='p-1 text-center'>
-                    <Project />
-                  </Col>
-                ) : (
-                  <Col className='p-1'>
-                    <div
-                      role='button'
-                      className='section__placeholder p-3 text-center'
-                    >
-                      + Add Projects Section
-                    </div>
-                  </Col>
-                )}
-              </Row>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { useState } from 'react';
+import { Link, BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
-              <Row className='section'>
-                <Col className='p-1'>
-                  <div
-                    role='button'
-                    className='section__placeholder p-3 text-center'
-                  >
-                    + Add Custom Section
-                  </div>
-                </Col>
-              </Row>
-            </Container>
+function LandingPageButton({ buttonText, navigateTo, callbackFn}){
+  const [hover, setHover] = useState(false);
+  const navigate = useNavigate();
+  return (
+    
+      <Col onClick={async ()=>{
+        let profileId = await callbackFn();
+        console.log(profileId);
+        navigate(`${navigateTo}/${profileId}`);
+      }} onMouseOver={()=>setHover(true)} onMouseLeave={()=>setHover(false)} className='landingPage__btn m-5' role='button'>
+            
+              <h1>{buttonText}</h1>
+              {hover && <h1>+</h1>}
+            
           </Col>
-          {/* <Col className='border bg-secondary text-center p-2 m-1'>
-            <h1>Preview</h1>
-          </Col> */}
-        </Row>
-      </Container>
-    </>
-  );
+    )
 }
 
-export default App;
+export default function App() {
+  
+  const createNewProfile = async ()=>{
+    try{
+      let result = await fetch('http://localhost:5000/api/v1/profile', {
+        method:'POST',
+        body:JSON.stringify({
+        label:'Profile'
+        }),
+          headers: {
+        "Content-Type": "application/json",
+        },
+      })
+      let data = await result.json();
+      console.log(data.id);
+      return data.id;
+    }
+    catch(err) {
+      console.error(err);
+      return null;
+    }
+    
+  }
+  return (
+    <>
+      <Container fluid>
+        <Row className='d-flex vh-100 text-center'>
+          <LandingPageButton buttonText='Create New Profile' navigateTo='/profile' callbackFn={createNewProfile}/>
+          <LandingPageButton buttonText='Use Existing Profile for New Resume' />
+        </Row>
+      </Container>
+      
+      </>
+    )
+}
