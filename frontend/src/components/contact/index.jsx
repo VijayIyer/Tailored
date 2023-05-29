294412cec15a60771311db80ecf279af7c2e795b
vijay <VijayIyer>
2023-05-29 12:27:02 -0400

    Passes modify, id, value fields to Field component in contact section

--- frontend/src/components/contact/index.jsx
+++ frontend/src/components/contact/index.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
 import {
   Container,
   Form,
@@ -15,13 +16,60 @@ import { BsFillPersonFill } from "react-icons/bs";
 import Field from "../utils/field";
 import "./index.css";
 const Contact = () => {
+  const [id, setId] = useState(null);
   const [name, setName] = useState(null);
   const [email, setEmail] = useState(null);
   const [phone, setPhone] = useState(null);
   const [location, setLocation] = useState(null);
-  const onSaveClick = (e) => {
+  const locationHook = useLocation();
+  const onSaveClick = async (e) => {
     e.preventDefault();
+    if(id){
+      fetch(`http://localhost:5000/api/v1/contact/${id}`, {
+        method:'POST',
+        body:JSON.stringify({
+          name:name, 
+          email:email, 
+          phone:phone, 
+          location:location
+        }),
+        headers:{
+          "content-type":"application/json"
+        }
+      })
+      .then(res=>res.json())
+      .then(data=>console.log(data))
+      .catch(err=>console.error(err))
+    }
   };
+  useEffect(()=>{
+    console.log(locationHook.state.profileId);
+  })
+  useEffect(()=>{
+    console.log(`new contact field has id - ${id}`);
+  }, [id])
+  const createContactInfo = async () =>{
+    try{
+      let result = await fetch('http://localhost:5000/api/v1/contact', {
+        method:'POST',
+        "headers":{
+          "content-type":"application/json"
+        },
+        body:JSON.stringify({
+          "profileId": locationHook.state.profileId
+        })
+      });
+      let data = await result.json();
+      setId(data.id);
+    }
+    catch(err) {
+      console.error(err);
+      return null;
+    }
+  }
+  useEffect(()=>{
+    createContactInfo();
+  }, []);
   return (
     <>
       <Accordion className='text-center' alwaysOpen flush>
@@ -33,19 +81,28 @@ const Contact = () => {
             <Container className='text-center'>
               <Row>
                 <Col>
-                  <Field placeholder='Enter Name'>
+                  <Field placeholder='Enter Name'
+                  id={id}
+                  value={name}
+                  modify={(id, name)=>setName(name)}>
                     <BsFillPersonFill />
                   </Field>
                 </Col>
               </Row>
               <Row>
                 <Col>
-                  <Field placeholder='Enter Email Address'>
+                  <Field placeholder='Enter Email Address'
+                  id={id}
+                  value={email}
+                  modify={(id, email)=>setEmail(email)}>
                     <AiOutlineMail />
                   </Field>
                 </Col>
                 <Col>
-                  <Field placeholder='Enter Phone No.'>
+                  <Field placeholder='Enter Phone No.'
+                  id={id}
+                  value={phone}
+                  modify={(id, phone)=>setPhone(phone)}>
                     <AiTwotonePhone />
                   </Field>
                 </Col>
@@ -53,7 +110,10 @@ const Contact = () => {
 
               <Row>
                 <Col>
-                  <Field placeholder='Location'>
+                  <Field placeholder='Location'
+                  id={id}
+                  value={location}
+                  modify={(id, location)=>setLocation(location)}>
                     <GoLocation />
                   </Field>
                 </Col>
