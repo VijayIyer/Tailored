5f3b9714c8c3d38f8f38c5933153fec677ffa285
vijay <VijayIyer>
2023-05-30 09:03:51 -0400

    Adds main route for Education

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,7 @@ app.use('/api/v1/user', require('./routes/user.routes'))
 app.use('/api/v1/auth', require('./routes/auth.routes'))
 app.use('/api/v1/profile', require('./routes/profile.routes'));
 app.use('/api/v1/contact', require('./routes/contact.routes'));
+app.use('/api/v1/education', require('./routes/education.routes'));
 app.get('/', (req,res)=>{
   res.status(200).json({msg: `Resume Builder Application! Use endpoints in format 'api/v1/* for queries'`})
 })
