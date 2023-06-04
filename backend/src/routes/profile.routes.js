8ac491ba4e6d49f53336e65bdd03dffca92f41a9
vijay <VijayIyer>
2023-06-04 07:42:00 -0400

    Adds method to get all profiles

--- backend/src/routes/profile.routes.js
+++ backend/src/routes/profile.routes.js
@@ -1,6 +1,7 @@
 const express  = require('express');
 const router = express.Router();
 const profileCtrl = require('../controllers/profile.controller');
-router.get('/:id', profileCtrl.get)
+router.get('/', profileCtrl.getAll);
+router.get('/:id', profileCtrl.get);
 router.post('/', profileCtrl.create);
 module.exports = router;
