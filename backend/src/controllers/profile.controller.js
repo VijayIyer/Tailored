616460209a8167ecfe5e0ed26c51d26c648d8fe7
vijay <VijayIyer>
2023-06-04 07:41:13 -0400

    Adds get by profile method in profile controller

--- backend/src/controllers/profile.controller.js
+++ backend/src/controllers/profile.controller.js
@@ -3,7 +3,17 @@ const { v4 } = require('uuid');
 const CTRL = {};
 
 CTRL.get = (req, res) => {
-  res.send(`getting profile with id ${req.params.id}`);
+		const id = req.params.id;
+  	console.log(`getting profile with id - ${id}`);
+  	Profile.findById({_id:id})
+  	.then(profiles=>res.status(200).json({ok:true, profiles}))
+  	.catch(err=>res.status(500).json({ok:false, err}))
+};
+CTRL.getAll = (req, res) => {
+		console.log(`getting all profiles`);
+  	Profile.find()
+  	.then(profiles=>res.status(200).json({ok:true, profiles}))
+  	.catch(err=>res.status(500).json({ok:false, err}))
 };
 
 
