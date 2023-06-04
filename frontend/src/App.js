a4d0be98183a80a14918742dc82dfd3c5a042684
vijay <VijayIyer>
2023-06-04 07:50:11 -0400

    Adds code for navigating to existing saved profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,21 @@ import { useState } from 'react';
 import { Link, BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 
-function LandingPageButton({ buttonText, navigateTo, callbackFn}){
+function LandingPageButton({ buttonText, navigateTo, callbackFn, symbol='+'}){
   const [hover, setHover] = useState(false);
   const navigate = useNavigate();
+  const getProfileAndNavigate = async ()=>{
+      let profileId = await callbackFn();
+      if(profileId) 
+        navigate(`${navigateTo}/${profileId}`, {state:{profileId:profileId}});
+      else navigate('/');
+  }
   return (
     
-      <Col onClick={async ()=>{
-        let profileId = await callbackFn();
-        console.log(profileId);
-        navigate(`${navigateTo}/${profileId}`, {state:{profileId:profileId}});
-      }} onMouseOver={()=>setHover(true)} onMouseLeave={()=>setHover(false)} className='landingPage__btn m-5' role='button'>
+      <Col onClick={getProfileAndNavigate} onMouseOver={()=>setHover(true)} onMouseLeave={()=>setHover(false)} className='landingPage__btn m-5' role='button'>
             
               <h1>{buttonText}</h1>
-              {hover && <h1>+</h1>}
+              {hover && <h1>{symbol}</h1>}
             
           </Col>
     )
@@ -24,6 +26,22 @@ function LandingPageButton({ buttonText, navigateTo, callbackFn}){
 
 export default function App() {
   
+  const getExistingProfiles = async () =>{
+    try{
+      let result = await fetch('http://localhost:5000/api/v1/profile', {
+        method:'GET',
+        headers:{
+          'content-type':'application/json'
+        }
+      });
+      let data = await result.json();
+      return data.profiles[0]._id;
+    }
+    catch(err){
+      console.error(err);
+      return null;
+    }
+  }
   const createNewProfile = async ()=>{
     try{
       let result = await fetch('http://localhost:5000/api/v1/profile', {
@@ -50,7 +68,7 @@ export default function App() {
       <Container fluid>
         <Row className='d-flex vh-100 text-center'>
           <LandingPageButton buttonText='Create New Profile' navigateTo='/profile' callbackFn={createNewProfile}/>
-          <LandingPageButton buttonText='Use Existing Profile for New Resume' />
+          <LandingPageButton symbol="->" buttonText='Use Existing Profile for New Resume' navigateTo='/profile' callbackFn={getExistingProfiles}/>
         </Row>
       </Container>
       
