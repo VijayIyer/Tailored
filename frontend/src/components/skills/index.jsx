ab207cde0f71e43cb6c4510a2fd057c5812e82ad
vijay <VijayIyer>
2023-06-05 11:26:26 -0400

    Uses skills data passed to it and adds backend code for saving skills to database

--- frontend/src/components/skills/index.jsx
+++ frontend/src/components/skills/index.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
 import { InputTags } from "react-bootstrap-tagsinput";
 import { WithContext as ReactTags } from 'react-tag-input';
 import { Container, Row, Col, Button, ListGroup } from "react-bootstrap";
@@ -6,12 +7,23 @@ import { FaSave } from 'react-icons/fa';
 import "react-bootstrap-tagsinput/dist/index.css";
 import './skills.css';
 import './index.css';
-const Skills = () => {
+const Skills = ({ data }) => {
   const [state, setState] = useState([]);
   const [tags, setTags] = useState([]);
+  const location = useLocation();
+  const profileId = location.state.profileId;
   const [savedSkills, setSavedSkills] = useState([]);
   const addSkills = () =>{
-    console.log(`saving skills to server`);
+    fetch(`http://localhost:5000/api/v1/skills/${profileId}`, {
+      method:'POST',
+      body:JSON.stringify({skills: tags} ),
+      headers:{
+        'content-type':'application/json'
+      }
+    })
+    .then(res=>res.json())
+    .then(data=>setSavedSkills(data.skills))
+    .catch(err=>console.error(err))
   }
    const handleAddition = (tag) => {
     console.log(tag);
@@ -20,6 +32,18 @@ const Skills = () => {
   const handleDelete = (i) => {
     setTags(tags.filter((tag, index) => index !== i));
   };
+  useEffect(()=>{
+    console.log(`saved skill data is  - ${data}`);
+    if(data) setSavedSkills(data);
+  }, [data])
+  useEffect(()=>{
+    setTags(savedSkills.map((skill)=>{
+      return ({
+        id:skill, 
+        text:skill
+      })
+    }))
+  }, [savedSkills]);
   return (
     <Container fluid>
       <Row><h3>Skills</h3></Row>
