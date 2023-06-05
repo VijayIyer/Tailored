129cddc540075658fb2ec97c6edb193183a0a703
vijay <VijayIyer>
2023-06-05 10:46:26 -0400

    Refactors App.js to navigate to both new profile and existing profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,77 +1,107 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import { useState } from 'react';
-import { Link, BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import { useState, useEffect } from "react";
+import {
+  Link,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
-function LandingPageButton({ buttonText, navigateTo, callbackFn, symbol='+'}){
+function LandingPageButton({
+  buttonText,
+  navigateTo,
+  symbol = "+",
+  children,
+}) {
   const [hover, setHover] = useState(false);
   const navigate = useNavigate();
-  const getProfileAndNavigate = async ()=>{
-      let profileId = await callbackFn();
-      if(profileId) 
-        navigate(`${navigateTo}/${profileId}`, {state:{profileId:profileId}});
-      else navigate('/');
-  }
+  useEffect(()=>{
+
+  }, [])
+ 
+  const createProfileAndNavigate = async () => {
+    try {
+      let result = await fetch("http://localhost:5000/api/v1/profile", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      let data = await result.json();
+      navigate(`${navigateTo}/${data.newProfile.uniqueId}`, { state: {profileId: data.newProfile.uniqueId} });
+    } catch (err) {
+      console.error(err);
+      navigate(`/profile/ErrorCreatingProfile`);
+    }
+  };
   return (
-    
-      <Col onClick={getProfileAndNavigate} onMouseOver={()=>setHover(true)} onMouseLeave={()=>setHover(false)} className='landingPage__btn m-5' role='button'>
-            
-              <h1>{buttonText}</h1>
-              {hover && <h1>{symbol}</h1>}
-            
-          </Col>
-    )
+    <Col
+      onClick={createProfileAndNavigate}
+      onMouseOver={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className='landingPage__btn m-5'
+      role='button'
+    >
+      <h1>{buttonText}</h1>
+      {hover && <h1>{symbol}</h1>}
+      {children}
+    </Col>
+  );
 }
 
 export default function App() {
-  
-  const getExistingProfiles = async () =>{
-    try{
-      let result = await fetch('http://localhost:5000/api/v1/profile', {
-        method:'GET',
-        headers:{
-          'content-type':'application/json'
-        }
-      });
-      let data = await result.json();
-      return data.profiles[0]._id;
-    }
-    catch(err){
-      console.error(err);
-      return null;
-    }
-  }
-  const createNewProfile = async ()=>{
-    try{
-      let result = await fetch('http://localhost:5000/api/v1/profile', {
-        method:'POST',
-        body:JSON.stringify({
-        label:'Profile'
-        }),
-          headers: {
-        "Content-Type": "application/json",
-        },
+  const [savedProfiles, setSavedProfiles] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    getExistingProfiles();
+  }, []);
+  const getExistingProfiles = () => {
+    fetch("http://localhost:5000/api/v1/profile", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSavedProfiles(data.profiles);
       })
-      let data = await result.json();
-      console.log(data.id);
-      return data.id;
-    }
-    catch(err) {
-      console.error(err);
-      return null;
-    }
-    
-  }
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  
   return (
     <>
       <Container fluid>
         <Row className='d-flex vh-100 text-center'>
-          <LandingPageButton buttonText='Create New Profile' navigateTo='/profile' callbackFn={createNewProfile}/>
-          <LandingPageButton symbol="->" buttonText='Use Existing Profile for New Resume' navigateTo='/profile' callbackFn={getExistingProfiles}/>
+          <LandingPageButton
+            buttonText='Create New Profile'
+            navigateTo='/profile'
+          />
+          <LandingPageButton
+            symbol='->'
+            buttonText='Use Existing Profile for New Resume'
+            navigateTo='/profile'
+          >
+            {savedProfiles.map((profile) => {
+              return (
+                <Row onClick={(e)=>{ 
+                  e.stopPropagation();
+                  navigate(`/profile/${profile.uniqueId}`, { state: {profileId:profile.uniqueId}})
+                }}
+                key={profile._id}
+                className='profile-listing'>
+                  <Col>{profile.label}</Col>
+                </Row>
+              );
+            })}
+          </LandingPageButton>
         </Row>
       </Container>
-      
-      </>
-    )
+    </>
+  );
 }
